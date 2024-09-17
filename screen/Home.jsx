6ef0f5f0ac67827ba7bb1bf56148acb7dd71f33e
shubham sharma <shubhamtech004@gmail.com>
2024-09-18 00:17:59 +0530

    all setup done

---
+++ screen/Home.jsx
@@ -0,0 +1,19 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+        <StatusBar style="auto"  backgroundColor='red'/>
+        <Text>Lets build something rock</Text>
+
+
+        
+
+      
+    </View>
+    
+  )
+}
+
+export default Home
